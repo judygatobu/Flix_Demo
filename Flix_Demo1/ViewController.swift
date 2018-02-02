fff19ab0b6362f5b1686c8ed8334da78cd195ca8
Judy Gatobu <judygatobu@Judys-MacBook-Air.local>
2018-02-02 00:26:27 -0500

    Initial Commit

---
+++ Flix_Demo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flix_Demo1
+//
+//  Created by Judy Gatobu on 2/2/18.
+//  Copyright © 2018 Judy Gatobu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
